a8c9572ec17d8b4a55021c845e799b39610c48dc
medinak044 <63528703+medinak044@users.noreply.github.com>
2022-07-03 21:29:19 -0500

    Added Models

---
+++ API/API/Models/AppUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace API.Models;
+
+public class AppUser: IdentityUser // Already includes (string) Id field
+{
+    //public int Id { get; set; }
+    public string DisplayName { get; set; }
+    public string Address { get; set; }
+}
